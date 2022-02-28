00a6107ca5c62603e321073ad980e951302d3b61
ChandiSandamini <96458@fhss.sjp.ac.lk>
2022-02-28 19:00:05 +0530

    initial commit

---
+++ src/yourdata.js
@@ -0,0 +1,195 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "chandi",
+  headerTagline: [
+    //Line 1 For Header
+    "THINK",
+    //Line 2 For Header
+    "DIFFERENT",
+    //Line 3 For Header
+    "AND DO IT",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    " I am passionate about my work.Because I love what I do, I have a steady source of motivation that drives me to do my best.In my last job, this passion led me to challenge myself daily and learn new skills that helped me to do better work.I soon became the go-to person for any design needs.",
+
+  //Contact Email
+  contactEmail: "kgcsandamini@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "First Project ", //Project Title - Add Your Project Title Here
+      para:
+        "My first project was to create a calculator, primarily using the python computer language.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1533109721025-d1ae7ee7c1e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fH",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/ChandiSandamini",
+    },
+    {
+      title: "Second Project ", //Project Title - Add Your Project Title Here
+      para:
+        "My second project is to create a payroll management system to calculate the payroll of an organization. This is also done using the python computer language.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1515261439133-0f6cfb098e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/ChandiSandamini",
+    },
+    {
+      title: "Third Project", //Project Title - Add Your Project Title Here
+      para:
+        "My third project is to create an online quiz software. This is done using the java computer language. It allows the user to test their general knowledge across a number of subject areas.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1598499305277-c0ff49a8a717?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/FHSS-USJ/final-project-ChandiSandamini",
+    },
+    {
+      title: "Fourth Project", //Project Title - Add Your Project Title Here
+      para:
+        " My fourth project is to create a mobile app. React - Native and JavaScript are used for this purpose. It focuses on several epidemics.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/FHSS-USJ/final-project-ChandiSandamini-1",
+    },
+    {
+      title: "Fifth Project", //Project Title - Add Your Project Title Here
+      para:
+        "My fifth project is to create the interface needed to create a mobile phone application. Figma has been used for this.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1515895309288-a3815ab7cf81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/FHSS-USJ/final-project-ChandiSandamini-1",
+    },
+    {
+      title: "Sixth Project", //Project Title - Add Your Project Title Here
+      para:
+        "My sixth project is creating a food delivery application that also uses React-Native, JavaScript, Expo.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/ChandiSandamini",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Hi, I'm Chandi. I am an undergraduate student in  the University of Sri Jayewardenepura.In addition, I have studied Sri Lanka Computer Competency license course, Computer Hardware, Computer Graphic Design courses in the education sector and courses at Edx and Cisco Academy.",
+  aboutParaTwo:
+    "I am currently studying computer languages ​​like Java, Python, JavaScript and would also like to learn other languages. Different systems are created using those languages, as well as mobile application applications as well as web pages.",
+  aboutParaThree:
+    "I love to do graphic design and use software like Adobe Photoshop, Adobe Illustrater, Coreldraw, indesign, Adobe Premiere Pro, Adobe XD, Figma.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1624996752380-8ec242e0f85d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para:
+        "HTML is the standard markup language for Web pages.With HTML you can create your own Website.HTML is easy to learn - You will enjoy it!",
+    },
+    {
+      img: cssIcon,
+      para:
+        "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.",
+    },
+    {
+      img: jsIcon,
+      para:
+        "JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries. ",
+    },
+    {
+      img: reactIcon,
+      para:
+        "React is a JavaScript library for building user interfaces.React is used to build single-page applications.React allows us to create reusable UI components.I use react and react Native to create websites and mobile phone applications.",
+    },
+    {
+      img: designIcon,
+      para:
+        "I use Adobe Photoshop, Adobe illustrator, Indesign, CorelDraw, Adobe Premiere Pro, Adobe XD, Figma to do graphic design.",
+    },
+    {
+      img: codeIcon,
+      para:
+        "I use Python, Java, Javascript computer languages ​​for recording and Pycharm Professional, Intellij Idea, Netbeans, VS code is the IDE used for that.",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "I love technology because it’s the triumph of the human intellect. The more I study and understand technology, the more I feel that nothing in the universe is out of reach. There’s even a great rush of power that comes from coding your first simple “Hello World” program. Getting it to run successfully, seeing those two words on the screen and knowing that YOU put them there just makes you think of the endless possibilities…",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/ChandiSandamini" },
+    {
+      img: codepenIcon,
+      url: "https://codepen.io/Senevirathna",
+    },
+    {
+      img: dribbbleIcon,
+      url: "https://dribbble.com/Senevirathna",
+    },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+
